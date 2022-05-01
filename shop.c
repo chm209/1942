fefc08548f0b5e19d1113b192140854e890b3790
chm209 <changmin7537@gmail.com>
2022-05-01 14:05:52 +0900

    중간 제출 파일 변경

--- shop.c
+++ shop.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+// 아이템 1, 치장 아이템
+// L: 리스트 / R: 미리보기
+// 
+// 보유 포인트 불러오기, 표시 필요
+// 이미 소유한 아이템은 리스트에 표시
+// 미리보기 기능은 컨트롤 불가
 
 void shop()
 {
-	// 포인트, 기존에 소유했던, 구매한 아이템 개수 DB 저장 필요
-
+	system("cls");
+	draw_content(3);
 }
